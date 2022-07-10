ff1180f3dfa034d2655b18748bf72a6fc9687758
Lukas Renggli <renggli@gmail.com>
2022-07-10 14:03:15 +0200

    Add `Token.newlineParser` to regexp benchmarks (we are doing quite well).

--- bin/regexp_benchmark.dart
+++ bin/regexp_benchmark.dart
@@ -67,4 +67,6 @@ void main() {
           (pattern('eE') & pattern('+-').optional() & digit().plus())
               .optional(),
       '1, -2, 3.4, -5.6, 7e8, 9E0, 0e+1, 2E-3, -4.567e-890');
+  compare(r'\n|\r\n?', Token.newlineParser(),
+      '1\n12\n123\n1234\n12345\n123456\n1234567\n12345678\r\n123456789\r');
 }
