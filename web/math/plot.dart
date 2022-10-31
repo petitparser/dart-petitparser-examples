c7c1b41b01540cc0bb829df3ccccf28b229f03c6
Lukas Renggli <renggli@gmail.com>
2022-10-31 22:50:16 +0100

    Resize canvas to window size

--- web/math/plot.dart
+++ web/math/plot.dart
@@ -4,40 +4,49 @@ import 'dart:html';
 import 'package:petitparser_examples/math.dart';
 
 class Viewport {
-  Viewport({
+  Viewport(
+    this.canvas, {
     required this.minX,
     required this.maxX,
     required this.minY,
     required this.maxY,
-    required this.width,
-    required this.height,
-  });
+  })  : context = canvas.context2D,
+        width = canvas.offsetWidth,
+        height = canvas.offsetHeight;
+
+  final CanvasElement canvas;
+  final CanvasRenderingContext2D context;
 
   final num minX;
   final num maxX;
-
   final num minY;
   final num maxY;
 
-  final int width;
-  final int height;
+  num width;
+  num height;
 
   /// Resizes the canvas.
-  void resize(CanvasElement canvas) {
-    final pixelRatio = window.devicePixelRatio;
+  void resize(num width, num height) {
+    final scale = window.devicePixelRatio;
+    this.width = width;
+    this.height = height;
     canvas.style.width = '${width}px';
     canvas.style.height = '${height}px';
-    canvas.width = (width * pixelRatio).truncate();
-    canvas.height = (height * pixelRatio).truncate();
+    canvas.width = (width * scale).truncate();
+    canvas.height = (height * scale).truncate();
+    context.scale(scale, scale);
   }
 
   /// Clears the viewport.
-  void clear(CanvasRenderingContext2D context) =>
-      context.clearRect(0, 0, width, height);
+  void clear() {
+    context.beginPath();
+    context.rect(0, 0, width, height);
+    context.clip();
+    context.clearRect(0, 0, width, height);
+  }
 
   /// Plots the grid and axis.
-  void grid(CanvasRenderingContext2D context,
-      {String axisStyle = 'black', String gridStyle = 'gray'}) {
+  void grid({String axisStyle = 'black', String gridStyle = 'gray'}) {
     context.lineWidth = 0.5;
     for (var x = minX.floor(); x <= maxX.ceil(); x++) {
       final pixelX = toPixelX(x);
@@ -58,13 +67,21 @@ class Viewport {
   }
 
   /// Plots a numeric function.
-  void plot(CanvasRenderingContext2D context, num Function(num x) function,
-      {String functionStyle = 'blue'}) {
+  void plot(num Function(num x) function, {String functionStyle = 'blue'}) {
     context.strokeStyle = functionStyle;
     context.lineWidth = 1.0;
     context.beginPath();
+    num lastY = double.infinity;
     for (var x = 0; x <= width; x++) {
-      context.lineTo(x, toPixelY(function(fromPixelX(x))));
+      final currentY = function(fromPixelX(x));
+      if (lastY.isInfinite ||
+          currentY.isInfinite ||
+          (lastY.sign != currentY.sign && (lastY - currentY).abs() > 100)) {
+        context.moveTo(x, toPixelY(currentY));
+      } else {
+        context.lineTo(x, toPixelY(currentY));
+      }
+      lastY = currentY;
     }
     context.stroke();
   }
@@ -85,14 +102,17 @@ class Viewport {
 final input = querySelector('#input')! as TextInputElement;
 final error = querySelector('#error')! as ParagraphElement;
 final canvas = querySelector('#canvas')! as CanvasElement;
-final context = canvas.getContext(
-  "2d",
-)! as CanvasRenderingContext2D;
 
-final viewport =
-    Viewport(minX: -5, maxX: 5, minY: -2.5, maxY: 2.5, width: 800, height: 400);
+final viewport = Viewport(canvas, minX: -5, maxX: 5, minY: -2.5, maxY: 2.5);
 
 Expression expression = Value(double.nan);
+
+void resize() {
+  final rect = canvas.parent?.getBoundingClientRect();
+  if (rect != null) {
+    viewport.resize(rect.width, rect.width / 2);
+  }
+}
 
 void update() {
   final source = input.value ?? '';
@@ -108,17 +128,18 @@ void update() {
 }
 
 void refresh(int tick) {
-  viewport.clear(context);
-  viewport.grid(context);
-  viewport.plot(context, (x) => expression.eval({'x': x, 't': tick}));
+  viewport.clear();
+  viewport.grid();
+  viewport.plot((x) => expression.eval({'x': x, 't': tick}));
 }
 
 void main() {
   if (window.location.hash.startsWith('#')) {
     input.value = Uri.decodeComponent(window.location.hash.substring(1));
   }
+  resize();
+  window.onResize.listen((event) => resize());
   update();
-  viewport.resize(canvas);
   input.onInput.listen((event) => update());
   Timer.periodic(const Duration(milliseconds: 1000 ~/ 30),
       (Timer timer) => refresh(timer.tick));
