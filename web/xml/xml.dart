51a37e0b8c692453be43b904e8cd2ce3b0567dee
Lukas Renggli <renggli@gmail.com>
2023-02-19 17:17:40 +0100

    Add XPath to the XML demo

--- web/xml/xml.dart
+++ web/xml/xml.dart
@@ -3,11 +3,14 @@ import 'dart:html';
 import 'package:more/collection.dart';
 import 'package:xml/xml.dart';
 import 'package:xml/xml_events.dart';
+import 'package:xml/xpath.dart';
 
-final input = querySelector('#input')! as TextAreaElement;
+final xmlInput = querySelector('#input') as TextAreaElement;
+final xpathInput = querySelector('#xpath') as TextInputElement;
 
-final saxOutput = querySelector('#output #sax')!;
-final domOutput = querySelector('#output #dom')!;
+final saxOutput = querySelector('#sax') as Element;
+final domOutput = querySelector('#dom') as Element;
+final xpathError = querySelector('#xpath-error') as Element;
 
 Element appendString(Element element, String object) {
   object
@@ -36,11 +39,11 @@ void appendSax(String type, [String? first, String? second]) {
 
 void update() {
   // Clear the output
-  domOutput.innerHtml = '';
-  saxOutput.innerHtml = '';
+  domOutput.innerText = '';
+  saxOutput.innerText = '';
 
   // Process the XML event stream
-  final eventStream = Stream.value(input.value ?? '')
+  final eventStream = Stream.value(xmlInput.value ?? '')
       .toXmlEvents(withLocation: true)
       .tapEachEvent(
         onCDATA: (event) => appendSax('CDATA', event.value),
@@ -67,16 +70,82 @@ void update() {
           appendLine(saxOutput, error.toString(), classes: ['error']));
 
   // Process the DOM stream
-  eventStream
-      .toXmlNodes()
-      .flatten()
-      .handleError((error) =>
-          appendLine(domOutput, error.toString(), classes: ['error']))
-      .where((node) => node is! XmlText || node.value.trim().isNotEmpty)
-      .listen((node) => appendLine(domOutput, node.toXmlString(pretty: true)));
+  eventStream.toXmlNodes().flatten().toList().then(
+        (elements) => updateDom(elements),
+        onError: (error) =>
+            appendLine(domOutput, error.toString(), classes: ['error']),
+      );
+}
+
+void updateDom(List<XmlNode> elements) {
+  final document = XmlDocument(elements);
+
+  final matches = <XmlNode>{};
+  try {
+    matches.addAll(document.xpath(xpathInput.value ?? ''));
+    xpathError.innerText = '';
+  } catch (error) {
+    xpathError.innerText = error.toString();
+  }
+
+  HtmlPrettyWriter(HtmlBuffer(domOutput), matches).visit(document);
+}
+
+class HtmlBuffer extends StringSink {
+  HtmlBuffer(Element root) {
+    stack.add(root);
+  }
+
+  final List<Node> stack = [];
+
+  void nest(String tag, void Function() function) {
+    final element = Element.tag(tag);
+    stack.last.append(element);
+    stack.add(element);
+    function();
+    stack.removeLast();
+  }
+
+  @override
+  void write(Object? object) {
+    object
+        .toString()
+        .split('\n')
+        .map<Node>((each) => Text(each))
+        .separatedBy(() => Element.br())
+        .forEach(stack.last.append);
+  }
+
+  @override
+  void writeAll(Iterable objects, [String separator = ""]) =>
+      throw UnimplementedError();
+
+  @override
+  void writeCharCode(int charCode) => throw UnimplementedError();
+
+  @override
+  void writeln([Object? object = ""]) => throw UnimplementedError();
+}
+
+class HtmlPrettyWriter extends XmlPrettyWriter {
+  HtmlPrettyWriter(this.htmlBuffer, this.matches) : super(htmlBuffer);
+
+  final HtmlBuffer htmlBuffer;
+
+  final Set<XmlNode> matches;
+
+  @override
+  void visit(XmlHasVisitor node) {
+    if (matches.contains(node)) {
+      htmlBuffer.nest('strong', () => super.visit(node));
+    } else {
+      super.visit(node);
+    }
+  }
 }
 
 void main() {
-  input.onInput.listen((event) => update());
+  xmlInput.onInput.listen((event) => update());
+  xpathInput.onInput.listen((event) => update());
   update();
 }
