0f37d60185c298d9ad41d309109d8c6f7ff89a11
Lukas Renggli <renggli@gmail.com>
2023-05-20 05:27:25 -0700

    Improve error reporting of Dart.

--- test/dart_file_tests.dart
+++ test/dart_file_tests.dart
@@ -3,23 +3,30 @@
 /// these files.
 import 'dart:io';
 
+import 'package:collection/collection.dart';
 import 'package:petitparser/petitparser.dart';
 import 'package:petitparser_examples/dart.dart';
 import 'package:test/test.dart';
 
 final grammar = DartGrammarDefinition().build();
 
-Function accept(Parser parser) => (input) => parser.parse(input).isSuccess;
+String addLineNumbers(String input) => input
+    .split('\n')
+    .mapIndexed(
+        (index, value) => '${(index + 1).toString().padLeft(4)}: $value')
+    .join('\n');
 
 void generateTests(String title, Directory root) {
   group(title, () {
     final files = root
         .listSync(recursive: true)
-        .where((file) => file is File && file.path.endsWith('.dart'))
-        .cast<File>();
+        .whereType<File>()
+        .where((file) => file.path.endsWith('.dart'));
     for (final file in files) {
-      test(file.path.substring(root.path.length + 1), () {
-        expect(file.readAsStringSync(), accept(grammar));
+      test(file.path.substring(root.path.length + 1), () async {
+        final input = await file.readAsString();
+        final result = grammar.parse(input);
+        if (result.isFailure) fail('$result\n\n${addLineNumbers(input)}');
       });
     }
   });
