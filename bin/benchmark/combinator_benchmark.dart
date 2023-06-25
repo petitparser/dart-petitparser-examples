ccd60155ebdbc5175dab19a6e931e40f2c33a9db
Lukas Renggli <renggli@gmail.com>
2023-06-25 09:50:04 +0200

    Better naming of combinator benchmarks.

--- bin/benchmark/combinator_benchmark.dart
+++ bin/benchmark/combinator_benchmark.dart
@@ -6,14 +6,14 @@ void main() {
   runChars('combinator - and', any().and(), 255);
   for (var i = 2; i < 10; i++) {
     final parsers = [...List.filled(i - 1, failure()), any()];
-    runChars('combinator - or$i', parsers.toChoiceParser(), 255);
+    runChars('combinator - or [$i]', parsers.toChoiceParser(), 255);
   }
   runChars('combinator - not', any().not(), 0);
   runChars('combinator - neg', any().neg(), 0);
   runChars('combinator - optional', any().optional(), 255);
   runChars('combinator - optionalWith', any().optionalWith('!'), 255);
   for (var i = 2; i < 256; i *= 2) {
-    runString('combinator - seq ${i.toString().padLeft(3, '0')}',
+    runString('combinator - seq [${i.toString().padLeft(3)}]',
         List.filled(i, any()).toSequenceParser());
   }
   runString('combinator - seq2', seq2(any(), any()));
@@ -24,14 +24,10 @@ void main() {
       'combinator - seq6', seq6(any(), any(), any(), any(), any(), any()));
   runString('combinator - seq7',
       seq7(any(), any(), any(), any(), any(), any(), any()));
-  runString(
-    'combinator - seq8',
-    seq8(any(), any(), any(), any(), any(), any(), any(), any()),
-  );
-  runString(
-    'combinator - seq9',
-    seq9(any(), any(), any(), any(), any(), any(), any(), any(), any()),
-  );
+  runString('combinator - seq8',
+      seq8(any(), any(), any(), any(), any(), any(), any(), any()));
+  runString('combinator - seq9',
+      seq9(any(), any(), any(), any(), any(), any(), any(), any(), any()));
   runChars('combinator - settable', any().settable(), 255);
   runChars('combinator - skip', any().skip(before: epsilon(), after: epsilon()),
       255);
